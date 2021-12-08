7acb596fcaba5ce0ea19c79d87fb42c4ce482e14
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-08 11:48:39 -0500

    Inside of my test i am overriding the built in fetch function with the dummy fetch function in which i created

--- testing/src/components/async/Async.test.js
+++ testing/src/components/async/Async.test.js
@@ -5,6 +5,10 @@ import Async from './Async'
 describe('Async component', () => {
 
   test('renders posts', async () => {
+    window.fetch = jest.fn()
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }]
+    })
     // Arrange
     render(<Async />)
     // Assert
