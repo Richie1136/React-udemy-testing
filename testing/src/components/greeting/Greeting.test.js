9bd888841a9613e7f6fae0f4d2cd5d85d20c5034
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-08 10:42:12 -0500

    Grouping tests together in the same suite

--- testing/src/components/greeting/Greeting.test.js
+++ testing/src/components/greeting/Greeting.test.js
@@ -1,11 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
+describe('Greeting component', () => {
 
-test('Learning how to automate tests in react', () => {
-  // Arrange
-  render(<Greeting />);
-  // Assert
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  test('Learning how to automate tests in react', () => {
+    // Arrange
+    render(<Greeting />);
+    // Assert
+    const linkElement = screen.getByText(/Hello World/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('Learning how to automate tests in react', () => {
+    // Arrange
+    render(<Greeting />);
+    // Assert
+    const welcomeElement = screen.getByText(/Welcome to my app/i);
+    expect(welcomeElement).toBeInTheDocument();
+  });
+})
+
